daa8a9fb1a469738b69335fdd105457fc45536cb
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-04-23 15:41:32 +0530

    [Khamisha]:Add-UC1-Create a Program with class and method

---
+++ OopsConcept/OopsConcept/ClassName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class ClassName
+    { 
+      
+        //Non parameterized method
+        public void PrintName()
+        {
+            Console.WriteLine("Khamisha Mittal");
+        }
+        public void PrintType(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
+
+    
+
---
+++ OopsConcept/OopsConcept/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OopsConcept
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ClassName name = new ClassName();
+            name.PrintName();
+            name.PrintType("Khamisha");
+            // It will hold the key until the key is pressed
+            Console.ReadKey();
+        }
+    }
+}
